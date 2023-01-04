3bf454d221cc955274d0205dc821920faa64d903
JunniorDionisioCasas <i1720115@continental.edu.pe>
2023-01-03 19:33:43 -0500

    Update user registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,11 +27,13 @@
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <small class="text-gray-500">Mínimo de 8 caracteres, que contengan números, símbolos y letras mayúsculas y minúsculas.</small>
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <small class="text-gray-500">Vuelva a introducir la contraseña.</small>
             </div>
 
             <div class="mt-4">
